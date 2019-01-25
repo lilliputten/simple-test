47b752ba6f9e41a74fe48f6c83f839b8da601401
lilliputten <lilliputten@yandex.ru>
2019-01-25 19:54:16 +0300

    Webhook test

---
+++ bitbucket-payload.php
@@ -0,0 +1,406 @@
+<?
+$payload = stdClass::__set_state(array(
+   'push' => stdClass::__set_state(array(/*{{{*/
+     'changes' => array (
+      0 => stdClass::__set_state(array(/*{{{*/
+         'forced' => false,
+         'old' => stdClass::__set_state(array(/*{{{*/
+           'name' => 'master',
+           'links' =>
+          stdClass::__set_state(array(
+             'commits' =>
+            stdClass::__set_state(array(
+               'href' => 'https://api.bitbucket.org/2.0/repositories/lilliputten/simple-test/commits/master',
+            )),
+             'self' =>
+            stdClass::__set_state(array(
+               'href' => 'https://api.bitbucket.org/2.0/repositories/lilliputten/simple-test/refs/branches/master',
+            )),
+             'html' =>
+            stdClass::__set_state(array(
+               'href' => 'https://bitbucket.org/lilliputten/simple-test/branch/master',
+            )),
+          )),
+           'default_merge_strategy' => 'merge_commit',
+           'merge_strategies' =>
+          array (
+            0 => 'merge_commit',
+            1 => 'squash',
+            2 => 'fast_forward',
+          ),
+           'type' => 'branch',
+           'target' =>
+          stdClass::__set_state(array(
+             'rendered' =>
+            stdClass::__set_state(array(
+            )),
+             'hash' => '33a2cd7e7329a129167fa57e16b33b7aa589b1e4',
+             'links' =>
+            stdClass::__set_state(array(
+               'self' =>
+              stdClass::__set_state(array(
+                 'href' => 'https://api.bitbucket.org/2.0/repositories/lilliputten/simple-test/commit/33a2cd7e7329a129167fa57e16b33b7aa589b1e4',
+              )),
+               'html' =>
+              stdClass::__set_state(array(
+                 'href' => 'https://bitbucket.org/lilliputten/simple-test/commits/33a2cd7e7329a129167fa57e16b33b7aa589b1e4',
+              )),
+            )),
+             'author' =>
+            stdClass::__set_state(array(
+               'raw' => 'lilliputten <lilliputten@yandex.ru>',
+               'type' => 'author',
+               'user' =>
+              stdClass::__set_state(array(
+                 'username' => 'lilliputten',
+                 'display_name' => 'Igor Ll',
+                 'account_id' => '557058:c53f49de-e259-4967-8414-ea28e53e9a0c',
+                 'links' =>
+                stdClass::__set_state(array(
+                   'self' =>
+                  stdClass::__set_state(array(
+                     'href' => 'https://api.bitbucket.org/2.0/users/lilliputten',
+                  )),
+                   'html' =>
+                  stdClass::__set_state(array(
+                     'href' => 'https://bitbucket.org/lilliputten/',
+                  )),
+                   'avatar' =>
+                  stdClass::__set_state(array(
+                     'href' => 'https://bitbucket.org/account/lilliputten/avatar/',
+                  )),
+                )),
+                 'nickname' => 'lilliputten',
+                 'type' => 'user',
+                 'uuid' => '{21a04279-8e18-4b9d-ad40-bd994c918c94}',
+              )),
+            )),
+             'summary' =>
+            stdClass::__set_state(array(
+               'raw' => 'Webhook test
+',
+               'markup' => 'markdown',
+               'html' => '<p>Webhook test</p>',
+               'type' => 'rendered',
+            )),
+             'parents' =>
+            array (
+              0 =>
+              stdClass::__set_state(array(
+                 'hash' => '439849fb3bef15aa3a9c2765f89807846ea03b0e',
+                 'type' => 'commit',
+                 'links' =>
+                stdClass::__set_state(array(
+                   'self' =>
+                  stdClass::__set_state(array(
+                     'href' => 'https://api.bitbucket.org/2.0/repositories/lilliputten/simple-test/commit/439849fb3bef15aa3a9c2765f89807846ea03b0e',
+                  )),
+                   'html' =>
+                  stdClass::__set_state(array(
+                     'href' => 'https://bitbucket.org/lilliputten/simple-test/commits/439849fb3bef15aa3a9c2765f89807846ea03b0e',
+                  )),
+                )),
+              )),
+            ),
+             'date' => '2019-01-25T16:20:45+00:00',
+             'message' => 'Webhook test
+',
+             'type' => 'commit',
+          )),
+        )),/*}}}*/
+         'links' => stdClass::__set_state(array(/*{{{*/
+           'commits' =>
+          stdClass::__set_state(array(
+             'href' => 'https://api.bitbucket.org/2.0/repositories/lilliputten/simple-test/commits?include=158496d4461c143afcaf315a9d05df5bee079e65&exclude=33a2cd7e7329a129167fa57e16b33b7aa589b1e4',
+          )),
+           'html' =>
+          stdClass::__set_state(array(
+             'href' => 'https://bitbucket.org/lilliputten/simple-test/branches/compare/158496d4461c143afcaf315a9d05df5bee079e65..33a2cd7e7329a129167fa57e16b33b7aa589b1e4',
+          )),
+           'diff' =>
+          stdClass::__set_state(array(
+             'href' => 'https://api.bitbucket.org/2.0/repositories/lilliputten/simple-test/diff/158496d4461c143afcaf315a9d05df5bee079e65..33a2cd7e7329a129167fa57e16b33b7aa589b1e4',
+          )),
+        )),/*}}}*/
+         'created' => false,
+         'commits' => array (/*{{{*/
+          0 =>
+          stdClass::__set_state(array(
+             'rendered' =>
+            stdClass::__set_state(array(
+            )),
+             'hash' => '158496d4461c143afcaf315a9d05df5bee079e65',
+             'links' =>
+            stdClass::__set_state(array(
+               'self' =>
+              stdClass::__set_state(array(
+                 'href' => 'https://api.bitbucket.org/2.0/repositories/lilliputten/simple-test/commit/158496d4461c143afcaf315a9d05df5bee079e65',
+              )),
+               'comments' =>
+              stdClass::__set_state(array(
+                 'href' => 'https://api.bitbucket.org/2.0/repositories/lilliputten/simple-test/commit/158496d4461c143afcaf315a9d05df5bee079e65/comments',
+              )),
+               'patch' =>
+              stdClass::__set_state(array(
+                 'href' => 'https://api.bitbucket.org/2.0/repositories/lilliputten/simple-test/patch/158496d4461c143afcaf315a9d05df5bee079e65',
+              )),
+               'html' =>
+              stdClass::__set_state(array(
+                 'href' => 'https://bitbucket.org/lilliputten/simple-test/commits/158496d4461c143afcaf315a9d05df5bee079e65',
+              )),
+               'diff' =>
+              stdClass::__set_state(array(
+                 'href' => 'https://api.bitbucket.org/2.0/repositories/lilliputten/simple-test/diff/158496d4461c143afcaf315a9d05df5bee079e65',
+              )),
+               'approve' =>
+              stdClass::__set_state(array(
+                 'href' => 'https://api.bitbucket.org/2.0/repositories/lilliputten/simple-test/commit/158496d4461c143afcaf315a9d05df5bee079e65/approve',
+              )),
+               'statuses' =>
+              stdClass::__set_state(array(
+                 'href' => 'https://api.bitbucket.org/2.0/repositories/lilliputten/simple-test/commit/158496d4461c143afcaf315a9d05df5bee079e65/statuses',
+              )),
+            )),
+             'author' =>
+            stdClass::__set_state(array(
+               'raw' => 'lilliputten <lilliputten@yandex.ru>',
+               'type' => 'author',
+               'user' =>
+              stdClass::__set_state(array(
+                 'username' => 'lilliputten',
+                 'display_name' => 'Igor Ll',
+                 'account_id' => '557058:c53f49de-e259-4967-8414-ea28e53e9a0c',
+                 'links' =>
+                stdClass::__set_state(array(
+                   'self' =>
+                  stdClass::__set_state(array(
+                     'href' => 'https://api.bitbucket.org/2.0/users/lilliputten',
+                  )),
+                   'html' =>
+                  stdClass::__set_state(array(
+                     'href' => 'https://bitbucket.org/lilliputten/',
+                  )),
+                   'avatar' =>
+                  stdClass::__set_state(array(
+                     'href' => 'https://bitbucket.org/account/lilliputten/avatar/',
+                  )),
+                )),
+                 'nickname' => 'lilliputten',
+                 'type' => 'user',
+                 'uuid' => '{21a04279-8e18-4b9d-ad40-bd994c918c94}',
+              )),
+            )),
+             'summary' =>
+            stdClass::__set_state(array(
+               'raw' => 'Webhook test
+',
+               'markup' => 'markdown',
+               'html' => '<p>Webhook test</p>',
+               'type' => 'rendered',
+            )),
+             'parents' =>
+            array (
+              0 =>
+              stdClass::__set_state(array(
+                 'hash' => '33a2cd7e7329a129167fa57e16b33b7aa589b1e4',
+                 'type' => 'commit',
+                 'links' =>
+                stdClass::__set_state(array(
+                   'self' =>
+                  stdClass::__set_state(array(
+                     'href' => 'https://api.bitbucket.org/2.0/repositories/lilliputten/simple-test/commit/33a2cd7e7329a129167fa57e16b33b7aa589b1e4',
+                  )),
+                   'html' =>
+                  stdClass::__set_state(array(
+                     'href' => 'https://bitbucket.org/lilliputten/simple-test/commits/33a2cd7e7329a129167fa57e16b33b7aa589b1e4',
+                  )),
+                )),
+              )),
+            ),
+             'date' => '2019-01-25T16:21:24+00:00',
+             'message' => 'Webhook test
+',
+             'type' => 'commit',
+          )),
+        ),/*}}}*/
+         'truncated' => false,
+         'closed' => false,
+         'new' => stdClass::__set_state(array(/*{{{*/
+           'name' => 'master',
+           'links' =>
+          stdClass::__set_state(array(
+             'commits' =>
+            stdClass::__set_state(array(
+               'href' => 'https://api.bitbucket.org/2.0/repositories/lilliputten/simple-test/commits/master',
+            )),
+             'self' =>
+            stdClass::__set_state(array(
+               'href' => 'https://api.bitbucket.org/2.0/repositories/lilliputten/simple-test/refs/branches/master',
+            )),
+             'html' =>
+            stdClass::__set_state(array(
+               'href' => 'https://bitbucket.org/lilliputten/simple-test/branch/master',
+            )),
+          )),
+           'default_merge_strategy' => 'merge_commit',
+           'merge_strategies' =>
+          array (
+            0 => 'merge_commit',
+            1 => 'squash',
+            2 => 'fast_forward',
+          ),
+           'type' => 'branch',
+           'target' =>
+          stdClass::__set_state(array(
+             'rendered' =>
+            stdClass::__set_state(array(
+            )),
+             'hash' => '158496d4461c143afcaf315a9d05df5bee079e65',
+             'links' =>
+            stdClass::__set_state(array(
+               'self' =>
+              stdClass::__set_state(array(
+                 'href' => 'https://api.bitbucket.org/2.0/repositories/lilliputten/simple-test/commit/158496d4461c143afcaf315a9d05df5bee079e65',
+              )),
+               'html' =>
+              stdClass::__set_state(array(
+                 'href' => 'https://bitbucket.org/lilliputten/simple-test/commits/158496d4461c143afcaf315a9d05df5bee079e65',
+              )),
+            )),
+             'author' =>
+            stdClass::__set_state(array(
+               'raw' => 'lilliputten <lilliputten@yandex.ru>',
+               'type' => 'author',
+               'user' =>
+              stdClass::__set_state(array(
+                 'username' => 'lilliputten',
+                 'display_name' => 'Igor Ll',
+                 'account_id' => '557058:c53f49de-e259-4967-8414-ea28e53e9a0c',
+                 'links' =>
+                stdClass::__set_state(array(
+                   'self' =>
+                  stdClass::__set_state(array(
+                     'href' => 'https://api.bitbucket.org/2.0/users/lilliputten',
+                  )),
+                   'html' =>
+                  stdClass::__set_state(array(
+                     'href' => 'https://bitbucket.org/lilliputten/',
+                  )),
+                   'avatar' =>
+                  stdClass::__set_state(array(
+                     'href' => 'https://bitbucket.org/account/lilliputten/avatar/',
+                  )),
+                )),
+                 'nickname' => 'lilliputten',
+                 'type' => 'user',
+                 'uuid' => '{21a04279-8e18-4b9d-ad40-bd994c918c94}',
+              )),
+            )),
+             'summary' =>
+            stdClass::__set_state(array(
+               'raw' => 'Webhook test
+',
+               'markup' => 'markdown',
+               'html' => '<p>Webhook test</p>',
+               'type' => 'rendered',
+            )),
+             'parents' =>
+            array (
+              0 =>
+              stdClass::__set_state(array(
+                 'hash' => '33a2cd7e7329a129167fa57e16b33b7aa589b1e4',
+                 'type' => 'commit',
+                 'links' =>
+                stdClass::__set_state(array(
+                   'self' =>
+                  stdClass::__set_state(array(
+                     'href' => 'https://api.bitbucket.org/2.0/repositories/lilliputten/simple-test/commit/33a2cd7e7329a129167fa57e16b33b7aa589b1e4',
+                  )),
+                   'html' =>
+                  stdClass::__set_state(array(
+                     'href' => 'https://bitbucket.org/lilliputten/simple-test/commits/33a2cd7e7329a129167fa57e16b33b7aa589b1e4',
+                  )),
+                )),
+              )),
+            ),
+             'date' => '2019-01-25T16:21:24+00:00',
+             'message' => 'Webhook test
+',
+             'type' => 'commit',
+          )),
+        )),/*}}}*/
+      )),/*}}}*/
+    ),
+  )),/*}}}*/
+   'actor' => stdClass::__set_state(array(/*{{{*/
+     'username' => 'lilliputten',
+     'display_name' => 'Igor Ll',
+     'account_id' => '557058:c53f49de-e259-4967-8414-ea28e53e9a0c',
+     'links' =>
+    stdClass::__set_state(array(
+       'self' =>
+      stdClass::__set_state(array(
+         'href' => 'https://api.bitbucket.org/2.0/users/lilliputten',
+      )),
+       'html' =>
+      stdClass::__set_state(array(
+         'href' => 'https://bitbucket.org/lilliputten/',
+      )),
+       'avatar' =>
+      stdClass::__set_state(array(
+         'href' => 'https://bitbucket.org/account/lilliputten/avatar/',
+      )),
+    )),
+     'nickname' => 'lilliputten',
+     'type' => 'user',
+     'uuid' => '{21a04279-8e18-4b9d-ad40-bd994c918c94}',
+  )),/*}}}*/
+   'repository' => stdClass::__set_state(array(/*{{{*/
+     'scm' => 'git',
+     'website' => '',
+     'name' => 'simple-test',
+     'links' =>
+    stdClass::__set_state(array(
+       'self' =>
+      stdClass::__set_state(array(
+         'href' => 'https://api.bitbucket.org/2.0/repositories/lilliputten/simple-test',
+      )),
+       'html' =>
+      stdClass::__set_state(array(
+         'href' => 'https://bitbucket.org/lilliputten/simple-test',
+      )),
+       'avatar' =>
+      stdClass::__set_state(array(
+         'href' => 'https://bytebucket.org/ravatar/%7Bd2f1dded-c021-44ea-a9b0-f5846a4fc403%7D?ts=php',
+      )),
+    )),
+     'full_name' => 'lilliputten/simple-test',
+     'owner' =>
+    stdClass::__set_state(array(
+       'username' => 'lilliputten',
+       'display_name' => 'Igor Ll',
+       'account_id' => '557058:c53f49de-e259-4967-8414-ea28e53e9a0c',
+       'links' =>
+      stdClass::__set_state(array(
+         'self' =>
+        stdClass::__set_state(array(
+           'href' => 'https://api.bitbucket.org/2.0/users/lilliputten',
+        )),
+         'html' =>
+        stdClass::__set_state(array(
+           'href' => 'https://bitbucket.org/lilliputten/',
+        )),
+         'avatar' =>
+        stdClass::__set_state(array(
+           'href' => 'https://bitbucket.org/account/lilliputten/avatar/',
+        )),
+      )),
+       'nickname' => 'lilliputten',
+       'type' => 'user',
+       'uuid' => '{21a04279-8e18-4b9d-ad40-bd994c918c94}',
+    )),
+     'type' => 'repository',
+     'is_private' => false,
+     'uuid' => '{d2f1dded-c021-44ea-a9b0-f5846a4fc403}',
+  )),/*}}}*/
+));
